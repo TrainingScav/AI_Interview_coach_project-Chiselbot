f901c47700b6f250e9a12c08f0226aa57f441f6f
sohee <son5839@naver.com>
2025-10-24 17:22:03 +0900

    add: ai관련설정추가, BaseEntity추가, 질문레벨별&공통모듈 전략패턴설계, InterviewQuestion Entity추가
    1. ai관련 설정추가
    - build.gradle 수정
    - application-dev ai설정추가(api-key, 모델지정)
    2. BaseEntity
    - main 메서드에 EnableJpaAuditing 어노테이션 추가
    3. 질문레벨별&공통모듈 전략패턴설계

--- chiselbot_api_server/src/main/java/com/coach/chiselbot/ChiselbotApplication.java
+++ chiselbot_api_server/src/main/java/com/coach/chiselbot/ChiselbotApplication.java
@@ -2,8 +2,11 @@ package com.coach.chiselbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+// BaseEntity 사용을 위해 어노테이션 추가 - sohee
 public class ChiselbotApplication {
 
 	public static void main(String[] args) {
