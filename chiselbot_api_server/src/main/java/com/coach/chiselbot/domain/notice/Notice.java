47f04305f7641d910303ccbc414ad8730f044d59
HHG_DEV <dnslwk123@naver.com>
2025-11-03 11:16:18 +0900

    관리자 공지사항 SSR 작업 1차 완료

---
+++ chiselbot_api_server/src/main/java/com/coach/chiselbot/domain/notice/Notice.java
@@ -0,0 +1,41 @@
+package com.coach.chiselbot.domain.notice;
+
+import com.coach.chiselbot._global.entity.BaseEntity;
+import com.coach.chiselbot.domain.admin.Admin;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "notice")
+@Builder
+public class Notice extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long noticeId;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Integer viewCount = 0;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean isVisible = true;
+
+    // 작성자 (관리자)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Admin admin;
+}
